694a9c7b0a527524e4f8ad6f2937984a80a8a3c0
Talha Yiğit YILDIRIM <turan7118@gmail.com>
2026-05-31 21:10:48 +0300

    vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,6 +2,7 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : undefined,
 });
 
 export default pool;
